2125ead7157384418125db5bcf40bc160bdbaaf8
b5027416 <b5027416@my.shu.ac.uk>
2019-04-22 23:28:03 +0100

    restricted access to selected pages

--- itServices/reviewApp/urls.py
+++ itServices/reviewApp/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path
+from users import views as user_views
+from django.contrib.auth import views as auth_views
 from . import views 
 
 urlpatterns =[
 
 path('', views.home, name='reviewApp-home'),
 path('about', views.about, name='reviewApp-about'), 
+path('register',user_views.register, name='register'),
+path('profile', user_views.profile, name='profile'),
 path('contact',views.contact, name='reviewApp-contact'),
 ##path('reviews',views.reviews, name='reviewApp-reviews'),
 path('report',views.report, name='reviewApp-report'),
-
 
 ]
 
